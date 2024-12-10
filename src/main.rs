6d97059e3fbb4e3977c214232db72abb473e4c58
hoopraCylindo <thomas.molleholm@chaos.com>
2024-12-10 11:38:57 +0100

    chore: Rename days

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-mod day_1;
-mod day_2;
-mod day_3;
-mod day_4;
-mod day_5;
-mod day_6;
-mod day_7;
-mod day_8;
-mod day_9;
+mod day_01;
+mod day_02;
+mod day_03;
+mod day_04;
+mod day_05;
+mod day_06;
+mod day_07;
+mod day_08;
+mod day_09;
 mod util;
 
 use std::io::stdin;
@@ -21,43 +21,43 @@ fn main() {
     println!("day {day}");
     match day {
         1 => {
-            println!("part 1: {}", day_1::solve_part_1());
-            println!("part 2: {}", day_1::solve_part_2());
+            println!("part 1: {}", day_01::solve_part_1());
+            println!("part 2: {}", day_01::solve_part_2());
         }
         2 => {
-            println!("part 1: {}", day_2::solve_part_1());
-            println!("part 2: {}", day_2::solve_part_2());
+            println!("part 1: {}", day_02::solve_part_1());
+            println!("part 2: {}", day_02::solve_part_2());
         }
         3 => {
-            println!("part 1: {}", day_3::solve_part_1());
-            println!("part 2: {}", day_3::solve_part_2());
+            println!("part 1: {}", day_03::solve_part_1());
+            println!("part 2: {}", day_03::solve_part_2());
         }
         4 => {
-            println!("part 1: {}", day_4::solve_part_1());
-            println!("part 2: {}", day_4::solve_part_2());
+            println!("part 1: {}", day_04::solve_part_1());
+            println!("part 2: {}", day_04::solve_part_2());
         }
         5 => {
-            println!("part 1: {}", day_5::solve_part_1());
-            println!("part 2: {}", day_5::solve_part_2());
+            println!("part 1: {}", day_05::solve_part_1());
+            println!("part 2: {}", day_05::solve_part_2());
         }
         6 => {
-            println!("part 1: {}", day_6::solve_part_1());
-            println!("part 2: {}", day_6::solve_part_2());
+            println!("part 1: {}", day_06::solve_part_1());
+            println!("part 2: {}", day_06::solve_part_2());
         }
         7 => {
-            println!("part 1: {}", day_7::solve_part_1());
-            println!("part 2: {}", day_7::solve_part_2());
+            println!("part 1: {}", day_07::solve_part_1());
+            println!("part 2: {}", day_07::solve_part_2());
         }
         8 => {
-            println!("part 1: {}", day_8::solve_part_1());
-            println!("part 2: {}", day_8::solve_part_2());
+            println!("part 1: {}", day_08::solve_part_1());
+            println!("part 2: {}", day_08::solve_part_2());
         }
         9 => {
-            let result = day_9::solve_part_1();
+            let result = day_09::solve_part_1();
             assert_eq!(result, 6242766523059);
             println!("part 1: {}", result);
 
-            let result = day_9::solve_part_2();
+            let result = day_09::solve_part_2();
             assert_eq!(result, 6272188244509);
             println!("part 2: {}", result);
         }
